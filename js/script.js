a976ae040bd935de61014c9539df4bd0491f273d
PitterPatterns <40621604+PitterPatterns@users.noreply.github.com>
2020-07-01 16:00:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,17 +6,6 @@ FSJS project 2 - List Filter and Pagination
 // Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
 
 var studL = document.getElementsByClassName('student-item cf');
 console.log(studL);
@@ -25,20 +14,6 @@ var numOfItem = 10;
 
 
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 /*This function will take any list and page# and hide all but 10. */
 
@@ -52,8 +27,8 @@ const showPage = (list, page) => {
          list[i].style.display = 'none';
       }
    }
-}
-
+};
+showPage(studL, 1);
 
 /*** 
    Create the `appendPageLinks function` to generate, append, and add 
@@ -62,36 +37,64 @@ const showPage = (list, page) => {
 
 const appendPageLinks = (list) => {
 
-   /* This will create a div element, give it a className of pagination, as well as append it to the div element with the class name of page */
-   let page = document.querySelector('.page');
-   let div = document.createElement('div');
+   /* 1. This wil get the exact pages needed while displaying only 10 at a time */
+   const totNum = Math.ceil(list.length/numOfItem);
+
+   /* 2. This will create a div element, give it a className of pagination, as well as append it to the div element with the class name of .page */
+   const  page = document.querySelector('.page');
+   const div = document.createElement('div');
       div.className = 'pagination';
       page.appendChild(div);
-   /* This will store the pagination links  */
-   let unOrd = document.createElement('ul');
+
+   /* 3. This will store the pagination links..in the .pagination "div"  */
+   const unOrd = document.createElement('ul');
       div.appendChild(unOrd);
 
-      /*Fuction*/
-   function calculate() {
-      let 
-   }
 
-   /*this wiil loop over/add 'li' & 'a' tags */
-   for (n = 0; n < list.length/10; n++) {
-      let li = document.createElement('li');
-      let a = document.createElement('a');
 
+   /* 4. this wiil loop over/add 'li' & 'a' tags....as well as set the value to the href, text, and class */
+   for (n = 0; n < totNum; n++) {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+         unOrd.appendChild(li);
+         li.appendChild(a);
+      };
+         a.href = '#'
+         a.textContent = n + 1;
+         if (n === 0) { 
+            a.className = "active";
+      };
       
-   }
+      
+         
+   /* This will add a "click" EventListener to every "a" element and call the showPage function when clicked */ 
+         a.addEventListener('click', (e) => {
+            for (i = 0; i < a.length; i++) {
+               e. target.classList.remove('active');
+            }
+               e.target.className = "active";
+
+               showPage(list, e.target.textContent);
+                                             })
+      }
+   
+  
+  
+  
+  
    
 
 
 
-  /* This willl call the showPage function when clicked */
-  a.addEventListener('click', () => {})
 
    
-}
+
+
+
+ 
+
+
+
 
 
 
